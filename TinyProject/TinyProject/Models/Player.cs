fdd2a7dc132a43a2b55aec9c770fa4ea4e9d9afe
VansteelandtZarin <zarin.vansteelandt@student.howest.be>
2019-11-16 11:10:11 +0100

    added player class

---
+++ TinyProject/TinyProject/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyProject.Models
+{
+    class Player
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int PlayerId { get; set; }
+        [JsonProperty(PropertyName = "first_name")]
+        public string FirstName { get; set; }
+        [JsonProperty(PropertyName = "last_name")]
+        public string LastName { get; set; }
+        [JsonProperty(PropertyName = "position")]
+        public string Position { get; set; }
+        [JsonProperty(PropertyName = "team")]
+        public Teams Team { get; set; }
+    }
+}
